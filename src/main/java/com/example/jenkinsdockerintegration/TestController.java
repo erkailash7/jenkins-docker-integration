2090db9b8858fb59464b624bc4de212085e4a992
Kailash <kailashsaini7@gmail.com>
2020-04-11 00:47:18 +0530

    second commit

---
+++ src/main/java/com/example/jenkinsdockerintegration/TestController.java
@@ -0,0 +1,15 @@
+package com.example.jenkinsdockerintegration;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/message")
+    public String getMessage(){
+        System.out.println("enkins-docker-integration-message SOP");
+        return "jenkins-docker-integration-message";
+
+    }
+}
